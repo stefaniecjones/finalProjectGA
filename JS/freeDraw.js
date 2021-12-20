1c0d4a8f46084ab2a87f3f86b4fbc672c6aa3a29
Stefanie Jones <stefaniecjones@hotmail.com>
2021-12-21 08:31:40 +1100

    update

---
+++ JS/freeDraw.js
@@ -0,0 +1,195 @@
+// FUNCTION - toggle tool buttons
+// CREATE FUNCTION - when one button is toggled, don't allow others to be toggled
+document.querySelector(".newRoute").addEventListener("click", toggleNewRoute);
+document.querySelector(".import").addEventListener("click", toggleImport);
+document.querySelector(".pen").addEventListener("click", togglePen);
+document.querySelector(".undo").addEventListener("click", toggleUndo);
+document.querySelector(".textBox").addEventListener("click", toggleTextBox);
+document.querySelector(".leftHand").addEventListener("click", toggleLeftHand);
+document.querySelector(".rightHand").addEventListener("click", toggleRightHand);
+document.querySelector(".leftFoot").addEventListener("click", toggleLeftFoot);
+document.querySelector(".rightFoot").addEventListener("click", toggleRightFoot);
+document.querySelector(".match").addEventListener("click", toggleMatch);
+document.querySelector(".bolt").addEventListener("click", toggleBolt);
+document.querySelector(".anchors").addEventListener("click", toggleAnchors);
+
+function toggleNewRoute() {
+    document.querySelector(".newRoute").classList.toggle("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    document.removeEventListener("mousedown", start);
+}
+function toggleImport() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.toggle("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    document.removeEventListener("mousedown", start);
+}
+function togglePen() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.toggle("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    // document.removeEventListener("mousedown", start);
+}
+function toggleUndo() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.toggle("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");    
+    document.removeEventListener("mousedown", start);
+}
+function toggleTextBox() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.toggle("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");    
+    document.removeEventListener("mousedown", start);
+}
+function toggleLeftHand() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.toggle("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    document.removeEventListener("mousedown", start);
+}
+function toggleRightHand() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.toggle("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    document.removeEventListener("mousedown", start);
+}
+function toggleLeftFoot() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.toggle("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    document.removeEventListener("mousedown", start);
+}
+function toggleRightFoot() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.toggle("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    document.removeEventListener("mousedown", start);
+}
+function toggleMatch() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.toggle("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    document.removeEventListener("mousedown", start);
+}
+function toggleBolt() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.toggle("redBorder");
+    document.querySelector(".anchors").classList.remove("redBorder");
+    document.removeEventListener("mousedown", start);
+}
+function toggleAnchors() {
+    document.querySelector(".newRoute").classList.remove("redBorder");
+    document.querySelector(".import").classList.remove("redBorder");
+    document.querySelector(".pen").classList.remove("redBorder");
+    document.querySelector(".undo").classList.remove("redBorder");
+    document.querySelector(".textBox").classList.remove("redBorder");
+    document.querySelector(".leftHand").classList.remove("redBorder");
+    document.querySelector(".rightHand").classList.remove("redBorder");
+    document.querySelector(".leftFoot").classList.remove("redBorder");
+    document.querySelector(".rightFoot").classList.remove("redBorder");
+    document.querySelector(".match").classList.remove("redBorder");
+    document.querySelector(".bolt").classList.remove("redBorder");
+    document.querySelector(".anchors").classList.toggle("redBorder");
+    document.removeEventListener("mousedown", start);
+}
--- JS/main.js
+++ JS/main.js
@@ -1,39 +1,118 @@
+// function to erase
+
+// function to clear all?
+
+// function to save to an svg?
+
+// function - when 'tools' button is clicked, show <aside> tag which contains all the tools
+
+
+// TOGGLE HAMBURGER MENU
+// locate hamburger image
+const hamburgerImg = document.querySelector(".hamburgerImg");
+const dropDownMenu = document.querySelector(".dropDownMenu");
+const xLogo = document.querySelector(".xLogo");
+
+// add "click" event listener to hamburger image
+hamburgerImg.addEventListener("click", toggleDropDownMenu);
+
+// function - when hamburger menu is clicked, toggle dropDownMenuVisible class
+function toggleDropDownMenu() {
+    dropDownMenu.classList.toggle("dropDownMenuVisible");
+    // change hamburger menu to x symbol
+//    .classList.toggle("xLogoVisible");
+}
+
+
+// TOGGLE TOOLS MENU
+// locate tool text and toolMenu
+const mainNavToolButton = document.querySelector(".mainNavTools");
+const dropDownToolButton = document.querySelector(".dropDownTools");
+const toolsMenu = document.querySelector(".toolsMenu");
+
+// add 'click' event listenener to tool text
+mainNavToolButton.addEventListener("click", toggleToolsMenu);
+dropDownToolButton.addEventListener("click", toggleToolsMenu);
+
+// function - when tool text is clicked, toggle toolMenuVisible
+function toggleToolsMenu() {
+ toolsMenu.classList.toggle("toolMenuVisible");
+}
+
+
+
+// *** FREE-DRAW SCRIPT *** 
+// set up function to allow finger to draw on mobile.
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
-let coord = {x: 0, y: 0};
+let coord = { x: 0, y: 0 };
+const penButton = document.querySelector(".pen");
+let canvasOn = false;
+// function to change all buttons to solid run colour when clicked
 
-document.addEventListener("mousedown", start);
+penButton.addEventListener("click", allowDraw);
 document.addEventListener("mouseup", stop);
 window.addEventListener("resize", resize);
+
+
+resize();
 
 function resize() {
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
 }
+function reposition(event) {
+    coord.x = event.clientX - canvas.offsetLeft;
+    coord.y = event.clientY - canvas.offsetTop;
+}
+function allowDraw() {
+// when clicked, change button color
+    // document.querySelector(".pen").classList.toggle("redBorder");
+    // document.addEventListener("mousedown", canvasSwitch);
+    document.addEventListener("mousedown", start);
+}
 
-resize();
+// function removeDraw() {
+//     penpenButton.style.border = "black solid 0.2rem";
+//     document.addEventListener("mouseup", stop);
+// }
+
+// CURRENTLY NOT WORKING - MAKING LINES JOIN TOGETHER
+// function canvasSwitch() {
+//     if (canvasOn = false) {
+//         stop();
+//     }
+//     else {
+//         start();
+//     }
+// }
 
 function start(event) {
     document.addEventListener("mousemove", draw);
     reposition(event);
-}
-
-function resposition(event) {
-    coord.x = event.clientX - canvas.offsetLeft;
-    coord.y = event.clientY - canvas.offsetTop;
+    // canvasOn = true;
 }
 
 function stop() {
     document.removeEventListener("mousemove", draw);
+    // canvasOn = false;
 }
 
 function draw(event) {
     ctx.beginPath();
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 2;
     ctx.lineCap = "round";
-    ctx.strokeStyle = "#ACD3ED";
+    ctx.strokeStyle = "black";
     ctx.moveTo(coord.x, coord.y);
     reposition(event);
     ctx.lineTo(coord.x, coord.y);
     ctx.stroke();
+}
+
+// add LH, RH, LF, RF images to be placed on canvas when buttons are clicked
+
+// add text to canvas - NOT WORKING
+function text() {
+    ctx.fillText("Hello World", 50, 50);
+    console.log(text);
 }
